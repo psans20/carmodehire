9327e184e4fdc05702172ae573f2da08bb43db20
UH <75499631+psans20@users.noreply.github.com>
2024-06-17 12:59:19 +0100

    car update

--- src/app/components/Hero.js
+++ src/app/components/Hero.js
@@ -18,7 +18,9 @@ export default function Hero(){
  <h2 className={`font-bold text-black text-3xl md:text-5xl ${oswald.className}`}>Hire for Weekend & Drive for 3 Days!</h2>
 <p className={`text-md font-sans opacity-70 md:px-36 ${oswald.className}`}>Not sure which car to choose from our selection? Let us help you find the one that captures your imagination. We'll speed through the booking process so you'll be on your way in no time.</p>
 <a href='tel:03335770272'><button className={`rounded-full border-2 border-blue-800 text-blue-800 font-semibold px-8 py-2 flex text-lg hover:bg-blue-800 hover:text-white duration-300 ${oswald.className}`} id='callNow'><FaPhone className='mr-4 mt-1'/>Book Sport Car Today</button></a>
-<img src="https://i.ibb.co/vQhjjxz/lamborghini-pic.png" className='z-20'/>
+<img src="https://www.motortrend.com/uploads/sites/10/2017/09/2018-audi-r8-coupe-angular-front.png" className='z-20 w-96 relative top-12'/>
         </div>
     )
 }
+
+//<img src="https://i.ibb.co/vQhjjxz/lamborghini-pic.png" className='z-20'/>
