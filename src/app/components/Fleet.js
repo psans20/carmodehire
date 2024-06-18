b0409c9e76d4da29e77f7813a6b4f88ef604d6ad
UH <75499631+psans20@users.noreply.github.com>
2024-06-18 10:01:56 +0100

    replace phone

--- src/app/components/Fleet.js
+++ src/app/components/Fleet.js
@@ -40,7 +40,7 @@ Elevate your driving experience with GTR Hire – where luxury meets performance
             </div>
 
             <div className={`flex flex-col items-center justify-center gap-2 text-center md:space-y-0 lg:flex-row ${questrial.className}`} id="carButton">
-              <a href='tel:03335770272'><button className="flex rounded-full border-2 border-blue-800 py-3 px-32 md:px-20 hover:bg-blue-800 hover:text-white duration-300"><FaPhoneSquareAlt className="mr-2 mt-1" />Call</button></a>
+              <a href='tel:03300985257'><button className="flex rounded-full border-2 border-blue-800 py-3 px-32 md:px-20 hover:bg-blue-800 hover:text-white duration-300"><FaPhoneSquareAlt className="mr-2 mt-1" />Call</button></a>
               <a href="https://wa.me/+447412765891" target="_blank" rel="noopener noreferrer"><button className="flex rounded-full border-2 border-blue-800 py-3 px-[105px] md:px-14 hover:bg-blue-800 hover:text-white duration-300"><FaWhatsapp className="mr-2 mt-1" />WhatsApp</button></a>
             </div>
           </div>
--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -87,7 +87,7 @@ export default function Footer(){
             <path
               d="M12 5.432l8.159 8.159c.03.03.06.058.091.086v6.198c0 1.035-.84 1.875-1.875 1.875H15a.75.75 0 01-.75-.75v-4.5a.75.75 0 00-.75-.75h-3a.75.75 0 00-.75.75V21a.75.75 0 01-.75.75H5.625a1.875 1.875 0 01-1.875-1.875v-6.198a2.29 2.29 0 00.091-.086L12 5.43z" />
           </svg>
-         West Yorkshire, Unit 2 Halifax Road Liversedge WF15 6JQ, England
+         Unit 2, Redcar Lane STEETON BD20 6QJ, England
         </p>
         <p className="mb-4 flex items-center justify-center md:justify-start">
           <svg
@@ -113,7 +113,7 @@ export default function Footer(){
               d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
               clip-rule="evenodd" />
           </svg>
-          +44 7412 765891
+         0330 098 5257
         </p>
        
       </div>
